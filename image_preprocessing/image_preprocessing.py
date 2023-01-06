ba7dc274b3df46b2c1129cfa767301acb83d734e
Karantoumanis <ekar.manolis@gmail.com>
2023-01-06 14:23:34 +0200

    add new image preprocessing method

--- image_preprocessing/image_preprocessing.py
+++ image_preprocessing/image_preprocessing.py
@@ -3,18 +3,20 @@ import cv2
 import inline as inline
 import matplotlib
 import numpy as np
-import skimage
-from skimage import io
 import matplotlib.pyplot as plt
 
 
-
+# https://www.youtube.com/watch?v=kSqxn6zGE0c
 def preprocessing():
     images_path= os.listdir('./images')
     for img in images_path:
         image = cv2.imread('./images/'+img, 1)
 
         imag2 = get_grayscale(image)
+        imag2 = get_scale(image)
+
+
+
         #Show image
         # cv2.imshow(image, img)
         # k = cv2.waitKey(0)
@@ -29,18 +31,12 @@ def preprocessing():
 
 
 def get_grayscale(image):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
 
+def get_HSV(image):
+    return cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
 
-
-
-
-
-
-
-
-
-
-
+def get_scale(image):
+    return  cv2.resize(image, None, fx=0.2, fy=0.2, interpolation=cv2.INTER_CUBIC)
 
 preprocessing()
