97fa0167b7925406d82f056fe5d3c7026759af5a
Vasilis Balafas <me@vasilis.pw>
2023-02-22 18:03:05 +0200

    Updates for image annotations

--- helpers/Annotations.py
+++ helpers/Annotations.py
@@ -1,4 +1,4 @@
-classes = myDict = {"Σκωρίαση": '0', "Τετράνυχος": '1', }
+classes = myDict = {"Σκωρίαση": '0', "Τετράνυχος": '1', 'Πράσινο σκουλήκι' : '2' }
 
 def coco_to_yolo(x1, y1, w, h, image_w, image_h):
     return [str(((2*x1 + w)/(2*image_w))) , str(((2*y1 + h)/(2*image_h))), str(w/image_w), str(h/image_h)]
