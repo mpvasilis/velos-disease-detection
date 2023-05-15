040f3d7f6907f90fbdeb03120cefa187b06081ba
Vasilis Balafas <me@vasilis.pw>
2023-05-15 17:17:11 +0300

    Updates for training

--- train.py
+++ train.py
@@ -3,27 +3,7 @@ from pathlib import Path
 from yolov5 import train
 
 def train_yolov5(dataset_path, checkpoint):
-    data_path = Path('preprocessing_folders/'+dataset_path)
-    yolov5_path = Path("yolov5")
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    weights_path = yolov5_path / "yolov5s.pt"
-    project_name = "velos_train_"+checkpoint
-
-    # Train the model
-    train.train(
-        str(data_path),
-        img_size=640,
-        batch_size=16,
-        epochs=100,
-        weights=weights_path,
-        project=project_name,
-        name="yolov5s_finetune",
-        device=device,
-        multi_scale=False,
-        evolve=False,
-        exist_ok=True,
-    )
+    train.run(imgsz=640, data='preprocessing_folders/'+dataset_path+'/dataset.yaml')
     return True
 
 # import fiftyone as fo
