f816893db12faf4257c9ae9cfeafc56620cff7f4
Vasilis Balafas <me@vasilis.pw>
2023-07-05 16:16:44 +0300

    Set inference with CUDA Set platdoc weights to Skoriasis

--- helpers/detect.py
+++ helpers/detect.py
@@ -8,19 +8,19 @@ class DiseaseDetection:
 
     def __init__(self,  images_to_detect, mission):
         self.images_to_detect = images_to_detect
-        self.model = yolov5.load(os.path.join('model', 'best.pt'), device=0)
+        self.model = yolov5.load(os.path.join('model', 'best.pt')) #, device=0
         self.mission = mission
 
 
     def detect(self):
-        self.model.conf = 0.25  # NMS confidence threshold
-        self.model.iou = 0.45  # NMS IoU threshold
+        self.model.conf = 0.15  # NMS confidence threshold
+        self.model.iou = 0.34  # NMS IoU threshold
         self.model.agnostic = False  # NMS class-agnostic
         self.model.multi_label = False  # NMS multiple labels per box
-        self.model.max_det = 1000  # maximum number of detections per image
+        self.model.max_det = 100  # maximum number of detections per image
 
         for i in range(30):
-            if "rust" in self.model.names[i] or "blight"  in self.model.names[i]:
+            if "rust" in self.model.names[i] or "blight" in self.model.names[i] or "rot" in self.model.names[i] or "spot" in self.model.names[i]:
                 self.model.names[i] = 'Skoriasis'
         results = self.model(self.images_to_detect)
         results.print()
